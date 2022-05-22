49499b9abad0689b3958cd12f5b4599077da3f70
Tarciso Bandeira <tarcisojb@gmail.com>
2022-05-22 11:43:07 -0300

    aaaaaaaaaaaaaaaaaaaaaa

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const runtimeScriptErrors = require('jsdom/lib/jsdom/living/helpers/runtime-script-errors');
+const Swal = require('sweetalert2');
 const Cliente = require('../modules/clientes');
 const router = express.Router();
+const alerta = require('../utils/alert');
 
 var login = false;
 var cpflogin;
@@ -35,42 +36,65 @@ router.post('/cliente/salvar', (req, res) => {
     var rg = req.body.rg;
     var cpf = req.body.cpf;
 
-    Cliente.findOne({
-        where: {
-            cpf : cpf
-        }
-    }).then(cliente => {
-        if(cliente == undefined){
-            Cliente.create({
-                nome: nome,
-                rg: rg,
-                cpf: cpf
-            }).then(() => {
+    if(rg.length == 12 && cpf.length == 14){
+        Cliente.findOne({
+            where: {
+                cpf : cpf
+            }
+        }).then(cliente => {
+            if(cliente == undefined){
+                Cliente.create({
+                    nome: nome,
+                    rg: rg,
+                    cpf: cpf
+                }).then(() => {
+                    res.redirect('/login');
+                })
+            }else{
+                Swal({
+                    position: 'top-end',
+                    icon: 'warning',
+                    title: 'Você não digitou o seu RG e/ou CPF',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
                 res.redirect('/login');
-            })
-        }else{
-            res.redirect('/login');
-        }
-    })
+            }
+        });
+    }else{
+        Swal({
+            position: 'top-end',
+            icon: 'warning',
+            title: 'Você não digitou o seu RG e/ou CPF',
+            showConfirmButton: false,
+            timer: 1500
+        });
+    }
 });
 
 router.post('/login/entrar', (req, res) => {
     var cpf = req.body.cpf;
-
-    Cliente.findOne({
-        where: {
-            cpf: cpf
-        }
-    }).then(cliente => {
-        if(cliente == undefined){
-            cpflogin = cpf;
-            res.redirect('/cadastro');
-        }else{
-            cli = cliente;
-            login = true;
-            res.redirect('/index');
-        }
-    });
+    console.log('1');
+    if(cpf.length == 14){
+        Cliente.findOne({
+            where: {
+                cpf: cpf
+            }
+        }).then(cliente => {
+            if(cliente == undefined){
+                cpflogin = cpf;
+                res.redirect('/cadastro');
+            }else{
+                cli = cliente;
+                login = true;
+                res.redirect('/index');
+            }
+        });
+    }else{
+        console.log('2');
+        alerta.Toast;
+        console.log('3');
+    } 
 });
 
 module.exports = router;
