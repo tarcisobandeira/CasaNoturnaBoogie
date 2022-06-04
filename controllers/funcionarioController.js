b27a8c87e50e45dddae4dc996d5af31a10fef7b0
Tarciso Bandeira <42774157+tarcisobandeira@users.noreply.github.com>
2022-06-03 21:37:31 -0300

    Add files via upload

--- controllers/funcionarioController.js
+++ controllers/funcionarioController.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const Cliente = require('../modules/clientes');
+const Evento = require('../modules/eventos');
 const Funcionarios = require('../modules/funcionarios');
+const Produto = require('../modules/produtos');
+const Usuario = require('../modules/usuarios');
 const Usuarios = require('../modules/usuarios');
 const router = express.Router();
 
@@ -47,15 +51,99 @@ router.post('/loginFun/entrar', (req, res) => {
             console.log('Senha Invalida.');
             res.redirect('/loginFun');
         }
-
-
-        
     });
 });
 
-function pass(usuar){
-    return usuar;
-}
+//cliente
+router.get('/clientes', (req, res) => {
+    Cliente.findAll({
+        order: [
+            ['nome', 'ASC']
+        ]
+    }).then(clientes => {
+        res.render('clientes/clienteF', {
+            clientes: clientes,
+            usuarioFun: usuar
+        });
+    });
+});
 
-module.exports = {pass(usuar)};
-module.exports = router, pass(usuar);
+//evento
+router.get('/eventos', (req, res) => {
+    Evento.findAll({
+        order: [
+            ['descricao', 'ASC']
+        ]
+    }).then(eventos => {
+        res.render('eventos/eventoF', {
+            eventos: eventos,
+            usuarioFun: usuar
+        });
+    });
+});
+
+//funcionario
+router.get('/funcionarios', (req, res) => {
+    Funcionarios.findAll({
+        order: [
+            ['nome', 'ASC']
+        ]
+    }).then(funcionarios => {
+        res.render('funcionarios/funcionarioF', {
+            funcionarios: funcionarios,
+            usuarioFun: usuar
+        });
+    });
+});
+
+//usuarios
+router.get('/usuarios', (req, res) => {
+    Usuario.findAll({
+        include: [{
+            model: Funcionarios,
+            require: true
+        }],
+        order: [
+            ['nome', 'ASC']
+        ]
+    }).then(usuarios => {
+        res.render('usuarios/usuarioF', {
+            usuarios: usuarios,
+            usuarioFun: usuar
+        });
+    });
+});
+
+//Produtos
+router.get('/produtos', (req, res) => {
+    Produto.findAll({
+        order: [
+            ['descricao', 'ASC']
+        ]
+    }).then(produtos => {
+        res.render('produtos/produtoF', {
+            produtos: produtos,
+            usuarioFun: usuar
+        });
+    });
+});
+
+//userLogado
+router.get('/usuarioLogado', (req, res) => {
+    Usuario.findOne({
+        where: {
+            nome: usuar.funcionario.nome
+        },
+        include: [{
+            model: Funcionarios,
+            require: true
+        }]
+    }).then(usuarios => {
+        res.render('usuarios/userLogin', {
+            usuarios: usuarios,
+            usuarioFun: usuarios
+        })
+    });
+});
+
+module.exports = router;
--- controllers/trocaTelaController.js
+++ controllers/trocaTelaController.js
@@ -6,69 +6,7 @@ const Usuario = require('../modules/usuarios');
 const router = express.Router();
 const fc = require('./funcionarioController');
 
-var user = fc.pass();
+var usuar = fc.pass();
 
-//cliente
-router.get('/clientes', (req, res) => {
-
-    console.log(user);
-
-    Cliente.findAll({
-        order: [
-            ['nome', 'ASC']
-        ]
-    }).then(clientes => {
-        res.render('clientes/clienteF', {
-            clientes: clientes,
-            usuarioFun: user
-        });
-    });
-});
-
-//evento
-router.get('/eventos', (req, res) => {
-    Evento.findAll({
-        order: [
-            ['descricao', 'ASC']
-        ]
-    }).then(eventos => {
-        res.render('eventos/eventoF', {
-            eventos: eventos,
-            usuarioFun: user
-        });
-    });
-});
-
-//funcionario
-router.get('/funcionarios', (req, res) => {
-    Funcionarios.findAll({
-        order: [
-            ['nome', 'ASC']
-        ]
-    }).then(funcionarios => {
-        res.render('funcionarios/funcionarioF', {
-            funcionarios: funcionarios,
-            usuarioFun: user
-        });
-    });
-});
-
-//usuarios
-router.get('/usuarios', (req, res) => {
-    Usuario.findAll({
-        include: [{
-            model: Funcionarios,
-            require: true
-        }],
-        order: [
-            ['nome', 'ASC']
-        ]
-    }).then(usuarios => {
-        res.render('usuarios/usuarioF', {
-            usuarios: usuarios,
-            usuarioFun: user
-        });
-    });
-});
 
 module.exports = router;
